817e9a631c4f03544f755cd950b1dbd7d8836d53
Ivan Vielkov <56674485+6a6kaBKedax@users.noreply.github.com>
2022-07-07 03:56:36 +0300

    writed bloc

--- lib/bloc/home_bloc/home_bloc.dart
+++ lib/bloc/home_bloc/home_bloc.dart
@@ -1,13 +1,47 @@
+import 'dart:async';
+
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:infinity_cached_scroll/data/models/photo_model.dart';
+import 'package:infinity_cached_scroll/res/consts/namespace_consts.dart';
+import 'package:infinity_cached_scroll/services/repository/repository.dart';
 
 part 'home_event.dart';
+
 part 'home_state.dart';
 
 class HomeBloc extends Bloc<HomeEvent, HomeState> {
-  HomeBloc() : super(HomeInitial()) {
-    on<HomeEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+  HomeBloc() : super(const HomeInitial()) {
+    on<HomeRequestEvent>(_onRequest);
+    on<HomeInitEvent>(_init);
+  }
+
+  static const int _postLimit = 20;
+
+  Future<void> _onRequest(HomeRequestEvent event, Emitter<HomeState> emit) async {
+    if (state.hasReachedMax) {
+      return;
+    }
+    try {
+      final List<PhotoModel> posts = await Repository.client.getPhotos(state.posts.length, _postLimit);
+      if (posts.isEmpty || state.posts.length > 100) {
+        HomeSuccessState(hasReachedMax: true, posts: state.posts);
+      } else {
+        HomeSuccessState(posts: List.of(state.posts)..addAll(posts), hasReachedMax: false);
+      }
+    } catch (e) {
+      logger.e(e);
+      emit(HomeErrorState(posts: state.posts));
+    }
+  }
+
+  Future<void> _init(HomeInitEvent event, Emitter<HomeState> emit) async {
+    try {
+      final posts = await Repository.client.getPhotos(state.posts.length, _postLimit);
+      return emit(HomeSuccessState(hasReachedMax: true, posts: posts));
+    } on Exception catch (e) {
+      logger.e(e);
+      emit(HomeErrorState(posts: state.posts));
+    }
   }
 }
--- lib/bloc/home_bloc/home_state.dart
+++ lib/bloc/home_bloc/home_state.dart
@@ -1,10 +1,42 @@
 part of 'home_bloc.dart';
 
 abstract class HomeState extends Equatable {
-  const HomeState();
+  const HomeState({
+    required this.posts,
+    required this.hasReachedMax,
+  });
+
+  final List<PhotoModel> posts;
+  final bool hasReachedMax;
+
+  @override
+  String toString() {
+    return '''HomeState {hasReachedMax: $hasReachedMax, posts: ${posts.length} }''';
+  }
 }
 
 class HomeInitial extends HomeState {
+  const HomeInitial() : super(hasReachedMax: false, posts: const []);
+
   @override
-  List<Object> get props => [];
+  List<Object?> get props => [posts, hasReachedMax];
+}
+
+class HomeSuccessState extends HomeState {
+  const HomeSuccessState({
+    required List<PhotoModel> posts,
+    required bool hasReachedMax,
+  }) : super(hasReachedMax: hasReachedMax, posts: posts);
+
+  @override
+  List<Object?> get props => [posts, hasReachedMax];
+}
+
+class HomeErrorState extends HomeState {
+  const HomeErrorState({
+    required List<PhotoModel> posts,
+  }) : super(hasReachedMax: true, posts: posts);
+
+  @override
+  List<Object?> get props => [posts, hasReachedMax];
 }
